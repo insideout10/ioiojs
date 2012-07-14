aebd34b4829502cddb385ec3419e36048e2d13c1
david@insideout.io <david@insideout.io>
2012-07-14 09:43:13 +0300

    add test for Fillify

--- test/fillify-test.js
+++ test/fillify-test.js
@@ -11,11 +11,21 @@ AsyncTestCase("FillifyTest", {
 
         $.each( [0, 1, 2], function ( index, item ) {
 
-            $('<div class="background"></div><div class="container"><div class="header"></div><div class="content"></div></div>')
+            $('<div class="section"></div><div class="background"></div><div class="container"><div class="header"></div><div class="content"></div></div></div>')
                 .appendTo( element );
 
         });
 
+        // set html and body margin and padding 0 in order for width to match in future comparisons.
+        $( 'html,body' )
+            .css( 'margin', 0 )
+            .css( 'padding', 0 );
+
+        // set a section that must be 100% of its window.
+        $('.section')
+            .css( 'width', '100%' );
+
+        // check that the fillify is created.
         assertNotNull(
             element.fillify({
                 selectors: {
@@ -27,7 +37,10 @@ AsyncTestCase("FillifyTest", {
             })
         );
 
-
+        // check that the correct widths are set.
+        var width = $(window).width();
+        assertEquals( width, $('.section').width() );
+        assertEquals( width, $('.background').width() );
     }
 
 });
